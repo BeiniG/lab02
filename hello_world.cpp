cc1e4d7f5758a0998ea2550e94d1ea93d1e27372
BeiniG <ivanvas1247@gmail.com>
2026-04-04 00:17:21 -0700

    Добавлены комментарии

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
 
+// Основная функция
 int main() {
+    // Переменная имени
     std::string name;
+    
+    // Запрос имени у пользователя
     std::cout << "Enter your name: ";
     std::cin >> name;
+    
+    // Вывод
     std::cout << "Hello world from " << name << std::endl;
-    return 0;
+    
+    return 0; // Завершение программы
 }
